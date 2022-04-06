6151b0d984cc2f48ec7d16efc3a2b0956f8be5a3
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 18:21:46 +0200

    added Validator for SaveVehicle request model

---
+++ CarRentalApplication.Backend/CarRentalApi.BusinessLayer/Validators/SaveVehicleRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using CarRentalApi.Shared.Models.Requests;
+using FluentValidation;
+
+namespace CarRentalApi.BusinessLayer.Validators
+{
+    public class SaveVehicleRequestValidator : AbstractValidator<SaveVehicleRequest>
+    {
+        public SaveVehicleRequestValidator()
+        {
+            RuleFor(v => v.Brand)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("can't register a vehicle with no brand");
+
+            RuleFor(v => v.Model)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("can't register a vehicle with no model");
+
+            RuleFor(v => v.Plate)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("can't register a vehicle with no plate");
+        }
+    }
+}
