1277f95e02c75720eaf4572ca3f0573c957de8f7
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 16:40:56 +0200

    added People controller (must be completed with delete methods)

---
+++ CarRentalApplication.Backend/CarRentalApi/Controllers/PeopleController.cs
@@ -0,0 +1,76 @@
+﻿using CarRentalApi.BusinessLayer.Services;
+using CarRentalApi.Shared.Models;
+using CarRentalApi.Shared.Models.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Produces("application/json")]
+public class PeopleController : ControllerBase
+{
+    private readonly IPeopleService peopleService;
+
+    public PeopleController(IPeopleService peopleService)
+    {
+        this.peopleService = peopleService;
+    }
+
+    [HttpGet("GetPeople")]
+    [ProducesResponseType(200, Type = typeof(List<Person>))]
+    [ProducesResponseType(404, Type = typeof(string))]
+    public async Task<IActionResult> GetPeople()
+    {
+        var people = await peopleService.GetPeopleAsync();
+        if (people == null)
+        {
+            return NotFound("No rows found");
+        }
+
+        return Ok(people);
+    }
+
+    [HttpGet("GetPerson")]
+    [ProducesResponseType(200, Type = typeof(Person))]
+    [ProducesResponseType(404, Type = typeof(string))]
+    public async Task<IActionResult> GetPerson(Guid id)
+    {
+        var person = await peopleService.GetPersonAsync(id);
+        if (person == null)
+        {
+            return NotFound("the person with the specified id wasn't found");
+        }
+
+        return Ok(person);
+    }
+
+    [HttpPost("SavePerson")]
+    [ProducesResponseType(200, Type = typeof(string))]
+    [ProducesResponseType(400, Type = typeof(string))]
+    public async Task<IActionResult> SavePerson([FromBody] SavePersonRequest request)
+    {
+        request.Id = null;
+        var person = await peopleService.SaveAsync(request);
+        if (person != null)
+        {
+            return Ok($"the person {person.FirstName} {person.LastName} was successfully registrated");
+        }
+
+        return BadRequest("problem occurred during registration");
+    }
+
+    [HttpPut("UpdatePerson")]
+    [ProducesResponseType(200, Type = typeof(string))]
+    [ProducesResponseType(400, Type = typeof(string))]
+    public async Task<IActionResult> UpdatePerson([FromBody] SavePersonRequest request)
+    {
+        var person = await peopleService.SaveAsync(request);
+        if (person != null)
+        {
+            return Ok($"the person {person.FirstName} {person.LastName} was successfully updated");
+        }
+
+        return BadRequest("problem occurred during updating");
+    }
+}
