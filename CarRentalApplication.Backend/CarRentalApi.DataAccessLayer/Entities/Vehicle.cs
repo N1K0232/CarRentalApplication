fb0b89503e51bef3abee04c90151c6951834b4b0
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 15:01:20 +0200

    added entities

---
+++ CarRentalApplication.Backend/CarRentalApi.DataAccessLayer/Entities/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using CarRentalApi.DataAccessLayer.Entities.Common;
+
+namespace CarRentalApi.DataAccessLayer.Entities;
+
+public class Vehicle : BaseEntity
+{
+    public string Brand { get; set; }
+
+    public string Model { get; set; }
+
+    public string Plate { get; set; }
+
+    public virtual ICollection<Reservation> Reservations { get; set; }
+}
