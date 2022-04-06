12b8e4fccc8d450b52e056adbd41b3399b46a7c1
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 18:25:52 +0200

    added methods to interface

--- CarRentalApplication.Backend/CarRentalApi.BusinessLayer/Services/IVehiclesService.cs
+++ CarRentalApplication.Backend/CarRentalApi.BusinessLayer/Services/IVehiclesService.cs
@@ -1,6 +1,12 @@
-﻿namespace CarRentalApi.BusinessLayer.Services;
+﻿using CarRentalApi.Shared.Models;
+using CarRentalApi.Shared.Models.Requests;
+
+namespace CarRentalApi.BusinessLayer.Services;
 
 public interface IVehiclesService
 {
-
+    Task DeleteAsync(Guid id);
+    Task DeleteAsync();
+    Task<Vehicle> GetVehicleAsync(Guid id);
+    Task<Vehicle> SaveAsync(SaveVehicleRequest request);
 }
