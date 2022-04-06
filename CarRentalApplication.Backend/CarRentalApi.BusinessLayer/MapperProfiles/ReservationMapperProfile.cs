666d1e31b538e4ff769639db8b041753f83cd26d
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 18:52:08 +0200

    added ReservationMapperProfile

---
+++ CarRentalApplication.Backend/CarRentalApi.BusinessLayer/MapperProfiles/ReservationMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CarRentalApi.Shared.Models;
+using CarRentalApi.Shared.Models.Requests;
+using Entities = CarRentalApi.DataAccessLayer.Entities;
+
+namespace CarRentalApi.BusinessLayer.MapperProfiles;
+
+public class ReservationMapperProfile : Profile
+{
+    public ReservationMapperProfile()
+    {
+        CreateMap<Entities.Reservation, Reservation>();
+        CreateMap<SaveReservationRequest, Entities.Reservation>();
+    }
+}
