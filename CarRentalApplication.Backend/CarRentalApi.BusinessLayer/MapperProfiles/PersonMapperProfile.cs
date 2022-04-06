be88d0923ec1097f6effdc91d902dc9b06156108
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 15:39:52 +0200

    added PersonMapperProflle

---
+++ CarRentalApplication.Backend/CarRentalApi.BusinessLayer/MapperProfiles/PersonMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CarRentalApi.Shared.Models;
+using CarRentalApi.Shared.Models.Requests;
+using Entities = CarRentalApi.DataAccessLayer.Entities;
+
+namespace CarRentalApi.BusinessLayer.MapperProfiles;
+
+public class PersonMapperProfile : Profile
+{
+    public PersonMapperProfile()
+    {
+        CreateMap<Entities.Person, Person>();
+        CreateMap<SavePersonRequest, Entities.Person>();
+    }
+}
