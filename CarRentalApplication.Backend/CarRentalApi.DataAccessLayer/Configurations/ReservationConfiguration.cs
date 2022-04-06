4b2be3e630e576fbe54c80531766d933bed2c43f
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 15:11:29 +0200

    added configurations

---
+++ CarRentalApplication.Backend/CarRentalApi.DataAccessLayer/Configurations/ReservationConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using CarRentalApi.DataAccessLayer.Configurations.Common;
+using CarRentalApi.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRentalApi.DataAccessLayer.Configurations;
+
+internal class ReservationConfiguration : BaseEntityConfiguration<Reservation>
+{
+    public override void Configure(EntityTypeBuilder<Reservation> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("Reservations");
+
+        builder.HasOne(r => r.Person)
+            .WithMany(p => p.Reservations)
+            .HasForeignKey(r => r.IdPerson)
+            .IsRequired();
+
+        builder.HasOne(r => r.Vehicle)
+            .WithMany(v => v.Reservations)
+            .HasForeignKey(r => r.IdVehicle)
+            .IsRequired();
+
+        builder.Property(r => r.Start)
+            .IsRequired();
+
+        builder.Property(r => r.Finish)
+            .IsRequired();
+    }
+}
---
+++ CarRentalApplication.Backend/CarRentalApi.DataAccessLayer/Configurations/VehicleConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using CarRentalApi.DataAccessLayer.Configurations.Common;
+using CarRentalApi.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRentalApi.DataAccessLayer.Configurations;
+
+internal class VehicleConfiguration : BaseEntityConfiguration<Vehicle>
+{
+    public override void Configure(EntityTypeBuilder<Vehicle> builder)
+    {
+        base.Configure(builder);
+
+        builder.ToTable("Vehicles");
+
+        builder.Property(v => v.Brand)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(v => v.Model)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(v => v.Plate)
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
