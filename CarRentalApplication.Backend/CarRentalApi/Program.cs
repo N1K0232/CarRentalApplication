fdc6af708a007bf48af6c154efc1f724f188b83f
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 16:41:32 +0200

    added services

--- CarRentalApplication.Backend/CarRentalApi/Program.cs
+++ CarRentalApplication.Backend/CarRentalApi/Program.cs
@@ -1,25 +1,55 @@
+using CarRentalApi.BusinessLayer.MapperProfiles;
+using CarRentalApi.BusinessLayer.Services;
+using CarRentalApi.BusinessLayer.Validators;
+using CarRentalApi.DataAccessLayer;
+using FluentValidation.AspNetCore;
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using TinyHelpers.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddProblemDetails();
+builder.Services.AddControllers()
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "CarRentalApi", Version = "v1" });
+});
+builder.Services.AddAutoMapper(typeof(PersonMapperProfile).Assembly);
+builder.Services.AddFluentValidation(options =>
+{
+    options.RegisterValidatorsFromAssemblyContaining<SavePersonRequestValidator>();
+});
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
+    options.UseSqlServer(connectionString, dbOptions =>
+    {
+        dbOptions.EnableRetryOnFailure(10, TimeSpan.FromSeconds(2), null);
+    });
+});
+builder.Services.AddScoped<IDataContext>(service => service.GetRequiredService<DataContext>());
+builder.Services.AddScoped<IReadOnlyDataContext>(service => service.GetRequiredService<DataContext>());
+
+builder.Services.AddScoped<IPeopleService, PeopleService>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+app.UseProblemDetails();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/v1/swagger.json", "CarRentalApi v1"));
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
