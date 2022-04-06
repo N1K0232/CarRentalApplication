0f4f14bbf1f4601b17f244f877dbaca943526f81
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-06 15:42:06 +0200

    added first service

---
+++ CarRentalApplication.Backend/CarRentalApi.BusinessLayer/Services/PeopleService.cs
@@ -0,0 +1,13 @@
+﻿using CarRentalApi.DataAccessLayer;
+
+namespace CarRentalApi.BusinessLayer.Services;
+
+public class PeopleService : IPeopleService
+{
+    private readonly IDataContext dataContext;
+
+    public PeopleService(IDataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+}
