ee6a2474c5df1004c9ca9932e153ec941fdff928
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-07 12:33:30 +0200

    this file manages all the links

---
+++ CarRentalApplication.Core/CarRentalApiClient/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace CarRentalApiClient
+{
+    public static class Constants
+    {
+        public const string BaseUrl = "https://localhost:7166/";
+        public const string SavePerson = "api/People/SavePerson";
+        public const string UpdatePerson = "api/People/UpdatePerson";
+    }
+}
