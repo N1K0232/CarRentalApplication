3b885c4f91ea5ff9a3b028cb20b822872ad3319d
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-07 12:31:14 +0200

    added Save and Update person method

---
+++ CarRentalApplication.Core/CarRentalApiClient/Core/IPeopleClient.cs
@@ -0,0 +1,9 @@
+﻿using CarRentalApi.Shared.Models.Requests;
+
+namespace CarRentalApiClient.Core;
+
+public interface IPeopleClient : IDisposable
+{
+    Task<string> SaveAsync(SavePersonRequest request);
+    Task<string> UpdateAsync(SavePersonRequest request);
+}
---
+++ CarRentalApplication.Core/CarRentalApiClient/Core/PeopleClient.cs
@@ -0,0 +1,57 @@
+﻿using CarRentalApi.Shared.Models.Requests;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace CarRentalApiClient.Core;
+
+public class PeopleClient : IPeopleClient, IDisposable
+{
+    private readonly HttpClient httpClient;
+
+    public PeopleClient(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+    ~PeopleClient()
+    {
+        Dispose(false);
+    }
+
+    public async Task<string> SaveAsync(SavePersonRequest request)
+    {
+        using var apiResponse = await httpClient.PostAsJsonAsync(Constants.SavePerson, request);
+        if (apiResponse.IsSuccessStatusCode)
+        {
+            var content = await apiResponse.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<string>(content);
+            return response;
+        }
+
+        return null;
+    }
+    public async Task<string> UpdateAsync(SavePersonRequest request)
+    {
+        using var apiResponse = await httpClient.PutAsJsonAsync(Constants.UpdatePerson, request);
+        if (apiResponse.IsSuccessStatusCode)
+        {
+            var content = await apiResponse.Content.ReadAsStringAsync();
+            var response = JsonConvert.DeserializeObject<string>(content);
+            return response;
+        }
+
+        return null;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    private void Dispose(bool disposing)
+    {
+        if (disposing && httpClient != null)
+        {
+            httpClient.Dispose();
+        }
+    }
+}
