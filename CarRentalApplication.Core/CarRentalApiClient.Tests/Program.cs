a6b87cd0a76d2580c01c37bb6fff29d97c410a02
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-07 12:34:34 +0200

    created console app for tests

---
+++ CarRentalApplication.Core/CarRentalApiClient.Tests/Program.cs
@@ -0,0 +1,23 @@
+﻿using CarRentalApi.Shared.Models.Requests;
+using CarRentalApiClient;
+using CarRentalApiClient.Core;
+using System.Net.Http.Headers;
+
+var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri(Constants.BaseUrl);
+httpClient.DefaultRequestHeaders.Accept.Clear();
+httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+using var peopleClient = new PeopleClient(httpClient);
+
+var request = new SavePersonRequest
+{
+    FirstName = "Nicola",
+    LastName = "Silvestri",
+    BirthDate = DateTime.Parse("22/10/2002"),
+    PhoneNumber = "3319907702",
+    EmailAddress = "ns.nicolasilvestri@gmail.com"
+};
+
+var response = await peopleClient.SaveAsync(request);
+
+Console.ReadLine();
