27f413fb0821f21c512330cd58d784ce444c7e81
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-04-07 12:35:06 +0200

    added DependencyInjection project

---
+++ CarRentalApplication.Core/CarRentalApiClient.DependencyInjection/HttpClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CarRentalApiClient.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Headers;
+
+namespace CarRentalApiClient.DependencyInjection;
+
+public static class HttpClientExtensions
+{
+    public static IHttpClientBuilder AddPersonClient(this IServiceCollection services)
+    {
+        return services.AddHttpClient<IPeopleClient, PeopleClient>(httpClient =>
+        {
+            httpClient.BaseAddress = new Uri(Constants.BaseUrl);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return new PeopleClient(httpClient);
+        });
+    }
+}
